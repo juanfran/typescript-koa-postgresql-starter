d0c42276f26d91a1d7929ba7624ed53e907c60b3
Juanfran <juanfran.ag@gmail.com>
2019-12-11 15:43:05 +0100

    pg node

--- src/server.ts
+++ src/server.ts
@@ -3,13 +3,28 @@ import Router from '@koa/router';
 import Koa from 'koa';
 import bodyParser from 'koa-bodyparser';
 import logger from 'koa-logger';
+import { Pool } from 'pg';
+
+import Config from './config';
 
 const app = new Koa();
 const router = new Router();
 
+const pool = new Pool({
+  database: Config.db.database,
+  host: Config.db.host,
+  password: Config.db.password,
+  port: Config.db.port,
+  user: Config.db.user,
+});
+
+
+
 router.get('/', async (ctx) => {
+  const result = await pool.query('SELECT NOW()');
+
   ctx.body = {
-    text: 'Hello World',
+    text: `Hello World ${result.rows[0].now}`,
   };
 });
 
